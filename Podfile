17e625227c9194adc283f6f824f257c57d817bca
k4russ <k4russ@gmail.com>
2018-11-09 15:08:50 +0300

    first commit

---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '12.0'
+use_frameworks!
+
+
+def all_pods 
+end
+
+target 'Grafficity' do
+  all_pods
+end
---
+++ grafficity/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  grafficity
+//
+//  Created by Виктор Краснов on 09/11/2018.
+//  Copyright © 2018 doubletapp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
