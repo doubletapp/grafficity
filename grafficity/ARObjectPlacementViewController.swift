259346381746f3e100641209aaa54f71c5bb843f
Artemy <artemy.antropov@gmail.com>
2018-11-09 17:41:31 +0300

    Initial

---
+++ grafficity/ARObjectPlacementViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ARObjectPlacementViewController.swift
+//  Grafficity
+//
+//  Created by Artemy Antropov on 09/11/2018.
+//  Copyright © 2018 doubletapp. All rights reserved.
+//
+
+import Foundation
