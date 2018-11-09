8cd196c081abb6fcd8fca50dc408570b687eaedc
k4russ <k4russ@gmail.com>
2018-11-10 01:29:53 +0300

    Adding scenes with distances

--- grafficity/ViewController.swift
+++ grafficity/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  grafficity
-//
-//  Created by Виктор Краснов on 09/11/2018.
-//  Copyright © 2018 doubletapp. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -15,6 +7,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
 
+        let arMapViewController = ARMapViewController()
+        present(arMapViewController, animated: true)
+    }
 }
 
