1485d4eaa4793d8d12a9f6793417e7ca684b792a
Hash <hash@shin.lan>
2018-11-11 03:40:00 +0300

    Graffity details

--- grafficity/map/graffity/GraffityViewController.swift
+++ grafficity/map/graffity/GraffityViewController.swift
@@ -11,6 +11,32 @@ import UIKit
 
 class GraffityViewController: UIViewController {
     
+    var record: GraffityRecord!
+	
+    @IBOutlet weak var previewImage: UIImageView!
+    @IBOutlet weak var authorTitle: UILabel!
+    @IBOutlet weak var graffityTitle: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var checkinLabel: UILabel!
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var backArrow: UIImageView! {
+        didSet{
+            let tapRecognizer = UITapGestureRecognizer(
+                target: self, action: #selector(didTapBack))
+            backArrow.isUserInteractionEnabled = true
+            backArrow.addGestureRecognizer(tapRecognizer)
+        }
+    }
     
+    override func viewDidLoad() {
+        graffityTitle.text = record.name
+        authorTitle.text = record.artist
+        previewImage.image = record.preview
+        ratingLabel.text = "\(Int.random(in: 1...999))"
+        checkinLabel.text = "\(Int.random(in: 0...250))"
+    }
     
+    @objc func didTapBack() {
+        self.dismiss(animated: true)
+    }
 }
